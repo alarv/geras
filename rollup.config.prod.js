b2c9a50b7026ba6a0a7da9210ec1004aa77bbf15
alex.arvanitidis <alex.arvanitidis@glovoapp.com>
2020-04-19 16:08:01 +0200

    feat: prod build

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,16 +1,13 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import { uglify } from 'rollup-plugin-uglify';
 import typescript from 'rollup-plugin-typescript2';
 import copy from 'rollup-plugin-copy-assets';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-
     //  Our games entry point (edit as required)
-    input: [
-        './src/game.ts'
-    ],
+    input: ['./src/game.ts'],
 
     //  Where the build file is to be generated.
     //  Most games being built for distribution can use iife as the module type.
@@ -20,12 +17,15 @@ export default {
         name: 'GerasGame',
         format: 'iife',
         sourcemap: true,
+        intro: 'var global = window;'
     },
 
     plugins: [
         copy({
             assets: [
                 "src/assets",
+                "src/index.html",
+                "src/style.css",
             ],
         }),
         //  Toggle the booleans here to enable / disable Phaser 3 features:
@@ -35,34 +35,37 @@ export default {
             'typeof EXPERIMENTAL': JSON.stringify(true),
             'typeof PLUGIN_CAMERA3D': JSON.stringify(false),
             'typeof PLUGIN_FBINSTANT': JSON.stringify(false),
-            'typeof FEATURE_SOUND': JSON.stringify(true)
+            'typeof FEATURE_SOUND': JSON.stringify(true),
         }),
 
         //  Parse our .ts source files
         resolve({
-            extensions: [ '.ts', '.tsx' ]
+            extensions: ['.ts'],
         }),
 
         //  We need to convert the Phaser 3 CJS modules into a format Rollup can use:
         commonjs({
             include: [
                 'node_modules/eventemitter3/**',
-                'node_modules/phaser/**'
+                'node_modules/phaser/**',
             ],
-            exclude: [ 
-                'node_modules/phaser/src/polyfills/requestAnimationFrame.js'
+            exclude: [
+                'node_modules/phaser/src/polyfills/requestAnimationFrame.js',
             ],
             sourceMap: false,
-            ignoreGlobal: true
+            ignoreGlobal: true,
         }),
 
         //  See https://www.npmjs.com/package/rollup-plugin-typescript2 for config options
-        typescript(),
+        typescript({
+            typescript: require('typescript'),
+            objectHashIgnoreUnknownHack: true,
+        }),
 
-        //  See https://www.npmjs.com/package/rollup-plugin-uglify for config options
-        uglify({
-            mangle: false
-        })
+        //  See https://github.com/TrySound/rollup-plugin-terser for config options
+        terser({
+            sourcemap: false,
 
-    ]
+        }),
+    ],
 };
